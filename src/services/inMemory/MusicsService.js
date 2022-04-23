31119e17d7f615bd45a75e10d54a651f088db26e
tpadimas <adimasputra799@gmail.com>
2022-04-23 14:31:32 +0700

    create SongsService

--- src/services/inMemory/MusicsService.js
+++ src/services/inMemory/MusicsService.js
@@ -1,6 +1,6 @@
 const nanoid = require("nanoid");
 
-class MusicsService {
+class SongsService {
   constructor() {
     this._songs = [];
   }
@@ -18,5 +18,55 @@ class MusicsService {
     };
 
     this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new Error("Failed to add song");
+    }
+
+    return id;
+  }
+
+  getSongs() {
+    return this._songs;
+  }
+
+  getSongById(id) {
+    const song = this._songs.filter((s) => s.id === id)[0];
+    if (!song) {
+      throw new Error("Song not found");
+    }
+    return song;
+  }
+
+  editSongById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error("Failed to Update Song. Id not found");
+    }
+
+    this._songs[index] = {
+      ...this._songs[index],
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+    };
+  }
+
+  deleteSongById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new Error("Failed to Delete Song. Id not found");
+    }
+
+    this._songs.splice(index, 1);
   }
 }
+
+module.exports = SongsService;
