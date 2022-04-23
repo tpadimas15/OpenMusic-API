2a7bf4d280553896ca7108ac79dbc351ad2a911d
tpadimas <adimasputra799@gmail.com>
2022-04-23 22:22:59 +0700

    registrering plugin

--- src/server.js
+++ src/server.js
@@ -1,18 +1,40 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+const albums = require("./api/albums");
+const songs = require("./api/songs");
+const AlbumsService = require("./services/inMemory/AlbumsService");
+const SongsService = require("./services/inMemory/SongsService");
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
 
   server.route(routes);
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
