0cd1da9a0327e110b7818e77b913afa354103a72
tpadimas <adimasputra799@gmail.com>
2022-04-25 20:21:47 +0700

    album got fixed

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -1,3 +1,5 @@
+const ClientError = require("../../exceptions/ClientError");
+
 class SongsHandler {
   constructor(service, validator) {
     this._service = service;
@@ -15,7 +17,7 @@ class SongsHandler {
       this._validator.validateSongPayload(request.payload);
       const { title, year, genre, performer, duration, albumId } = request.payload;
 
-      const songId = this._service.addNote({ title, year, genre, performer, duration, albumId });
+      const songId = this._service.addSong({ title, year, genre, performer, duration, albumId });
 
       const response = h.response({
         status: "success",
