57a543a31b4b138031dec085f1834fb95357ebb3
tpadimas <adimasputra799@gmail.com>
2022-04-26 23:24:21 +0700

    fixing bug migration

--- migrations/1650952855991_create-table-albums.js
+++ migrations/1650952855991_create-table-albums.js
@@ -23,8 +23,42 @@ exports.up = (pgm) => {
       notNull: true,
     },
   });
+  pgm.createTable("songs", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "TEXT",
+      notNull: true,
+    },
+    year: {
+      type: "INTEGER",
+      notNull: true,
+    },
+    performer: {
+      type: "TEXT",
+      notNull: true,
+    },
+    genre: {
+      type: "TEXT",
+      notNull: false,
+    },
+    duration: {
+      type: "INTEGER",
+      notNull: false,
+    },
+    inserted_at: {
+      type: "TEXT",
+      notNull: true,
+    },
+    updated_at: {
+      type: "TEXT",
+      notNull: true,
+    },
+  });
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable("albums");
+  pgm.dropTable("albums", "songs");
 };
