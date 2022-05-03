7d51c679ec9f1507f79eb2b890f21c09fbb85546
tpadimas <adimasputra799@gmail.com>
2022-05-04 02:00:49 +0700

    typing error

--- src/validator/playlists/index.js
+++ src/validator/playlists/index.js
@@ -2,7 +2,7 @@ const InvariantError = require("../../exceptions/InvariantError");
 const { PlaylistPayloadSchema } = require("./schema");
 
 const PlaylistsValidator = {
-  validatePlaylistsPayload: (payload) => {
+  validatePlaylistPayload: (payload) => {
     const validationResult = PlaylistPayloadSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
